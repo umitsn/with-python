549193513d51292eaa47ee15ea232b78be2d1494
umitsn <49841290+umitsn@users.noreply.github.com>
2019-05-01 16:08:47 +0300

    Update kaprekar-2.py

--- python-kaprekar/kaprekar-2.py
+++ python-kaprekar/kaprekar-2.py
@@ -21,7 +21,7 @@ def islem(n,m):
     fark = Smax-Smin
     if(fark < 1000):
         fark=fark*10
-    txt = "%s.Adım = Sayi : %s Max : %s Min : %s Fark : %s"%(m,sayi,Smax,Smin,fark)
+    txt = "%s.Adım = Sayi : %s Max : %s Min : %s Fark : %s"%(m,sayi,Smax,str(Smin).ljust(4),fark)
     print(txt)
     return fark
 
@@ -36,7 +36,19 @@ def goster(k):
                 durum = False
             else:
                 k = ns
-                n +=1
-        print(a,n)
-          
+                n +=1      
 goster(3765)
+
+"""
+Örneğin ekran çıktısı : 
+>>> 
+====== RESTART: \umitsen\Desktop\python-kaprekar\kaprekar-2.py ======
+1.Adım = Sayi : 3765 Max : 7653 Min : 3567 Fark : 4086
+2.Adım = Sayi : 4086 Max : 8640 Min : 468  Fark : 8172
+3.Adım = Sayi : 8172 Max : 8721 Min : 1278 Fark : 7443
+4.Adım = Sayi : 7443 Max : 7443 Min : 3447 Fark : 3996
+5.Adım = Sayi : 3996 Max : 9963 Min : 3699 Fark : 6264
+6.Adım = Sayi : 6264 Max : 6642 Min : 2466 Fark : 4176
+7.Adım = Sayi : 4176 Max : 7641 Min : 1467 Fark : 6174
+>>> 
+"""
