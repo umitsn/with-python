b43b5604dab7c5da7b6f6725533e4a964c91c91b
umitsn <49841290+umitsn@users.noreply.github.com>
2019-05-01 16:19:54 +0300

    Update kaprekar-1.py

--- python-kaprekar/kaprekar-1.py
+++ python-kaprekar/kaprekar-1.py
@@ -24,7 +24,7 @@ def islem(n,m):
     fark = Smax-Smin
     if(fark < 1000):
         fark=fark*10
-    txt = "%s.Adım = Sayi : %s Max : %s Min : %s Fark : %s"%(m,sayi,Smax,Smin,fark)
+    txt = "%s.Adım = Sayi : %s Max : %s Min : %s Fark : %s"%(m,sayi,Smax,str(Smin).ljust(4),fark)
     print(txt)
     return fark
 
@@ -43,3 +43,19 @@ def goster(k):
           
 goster(3765)
 
+"""
+Ekran çıktısı : 
+>>>
+====== RESTART: \umitsen\Desktop\python-kaprekar\kaprekar-1.py ======
+1.Adım = Sayi : 3765 Max : 7653 Min : 3567 Fark : 4086
+2.Adım = Sayi : 4086 Max : 8640 Min : 468  Fark : 8172
+3.Adım = Sayi : 8172 Max : 8721 Min : 1278 Fark : 7443
+4.Adım = Sayi : 7443 Max : 7443 Min : 3447 Fark : 3996
+5.Adım = Sayi : 3996 Max : 9963 Min : 3699 Fark : 6264
+6.Adım = Sayi : 6264 Max : 6642 Min : 2466 Fark : 4176
+7.Adım = Sayi : 4176 Max : 7641 Min : 1467 Fark : 6174
+8.Adım = Sayi : 6174 Max : 7641 Min : 1467 Fark : 6174
+>>>
+NOT : kaprekar sabitine 7. adımda ulaştı fakar bir öncekiyle eşit olduğunu anlması için bir adım daha ilerletti ve 
+sonucun eşitlendiğini görünce 9. adıma geçmeden kod çalışmasını durdurdu. Yani işlem 7 adım sürdü...
+"""
