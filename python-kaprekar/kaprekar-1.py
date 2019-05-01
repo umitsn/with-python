ef80237f9e85640080d75204b8567775e0098d50
umitsn <49841290+umitsn@users.noreply.github.com>
2019-05-01 15:49:26 +0300

    Add files via upload

---
+++ python-kaprekar/kaprekar-1.py
@@ -0,0 +1,45 @@
+"""
+umit sen - izmir - mayıs 2019 - umitsen.wordpress.com
+Girilen herhangi bir dört basamaklı sayının kaprekar sabiti ile sonuçlandığını gösterir.
+
+"""
+
+def kontrol(n):
+    sayi = str(n)
+    liste = [int(x) for x in sayi]
+    if (liste.count(liste[0])==4):
+        print("Girilen dört rakam birbirine eşit olamaz.")
+        return False
+    else:
+        return True
+    
+def islem(n,m):
+    sayi = str(n)
+    liste = [int(x) for x in sayi]
+    min2max = sorted(liste)
+    max2min = min2max[::-1]
+
+    Smin = int("%s%s%s%s"%(min2max[0],min2max[1],min2max[2],min2max[3]))
+    Smax = int("%s%s%s%s"%(max2min[0],max2min[1],max2min[2],max2min[3]))
+    fark = Smax-Smin
+    if(fark < 1000):
+        fark=fark*10
+    txt = "%s.Adım = Sayi : %s Max : %s Min : %s Fark : %s"%(m,sayi,Smax,Smin,fark)
+    print(txt)
+    return fark
+
+def goster(k):
+    if kontrol(k):
+        a = k
+        durum = True
+        n=1
+        while durum:
+            ns = islem(k,n)
+            if(ns == k):
+                durum = False
+            else:
+                k = ns
+                n +=1
+          
+goster(3765)
+
---
+++ python-kaprekar/kaprekar-3.py
@@ -0,0 +1,45 @@
+"""
+umit sen - izmir - mayıs 2019 - umitsen.wordpress.com
+1000'den 9999'a kadar (tüm rakamları birbirine eşit olmayan) sayılar için bu sayıların  
+kaç adımda kaprekar sayısına ulaştığını hesaplar ve ekrana yazdırır.
+"""
+def kontrol(n):
+    sayi = str(n)
+    liste = [int(x) for x in sayi]
+    if (liste.count(liste[0])==4):
+        return False
+    else:
+        return True
+    
+def islem(n):
+    sayi = str(n)
+    liste = [int(x) for x in sayi]
+    min2max = sorted(liste)
+    max2min = min2max[::-1]
+
+    Smin = int("%s%s%s%s"%(min2max[0],min2max[1],min2max[2],min2max[3]))
+    Smax = int("%s%s%s%s"%(max2min[0],max2min[1],max2min[2],max2min[3]))
+    fark = Smax-Smin
+    if(fark < 1000):
+        fark=fark*10
+    return fark
+
+for i in range(1000,9999+1):
+    if kontrol(i):
+        a = i
+        durum = True
+        n=1
+
+        while durum:
+            ns = islem(i)
+            if(ns == 6174):
+                durum = False
+            else:
+                i = ns
+                n +=1
+        print(a,n)
+
+
+          
+
+
