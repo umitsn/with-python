4351b8b1a07aeca306dcd395c050cc73754555d8
umitsn <49841290+umitsn@users.noreply.github.com>
2021-09-06 20:28:11 +0300

    Add files via upload

---
+++ faktoriyelCarpanlari/faktoriyelSayisiCarpanlari.py
@@ -0,0 +1,26 @@
+"""
+ümit şen 09.2021 bircalismaningunlugu@gmail.com
+n! sayısının sonunda kaç 0 vardır ya da 
+n! içinde kaç tane x tamsayı çarpanı vardır
+sorusuna çözüm içindir.
+Bolen sayı 5 seçilirse n! sayısının sonunda kaç sıfır olduğu 
+bulunmuş olur.
+"""
+
+sayi = 51
+bolen = 3
+
+durum = True
+liste = []
+tmp = sayi
+while durum:
+    bolum = tmp // bolen
+    if bolum >= bolen:
+        liste.append(bolum)
+        tmp = bolum
+    else:
+        liste.append(bolum)
+        durum = False
+
+print(f'Girilen sayı : {sayi} \nÇarpan : {bolen}')       
+print(f'Bölüm Listesi : {liste} \n{sayi}! sayısı içinde toplam {sum(liste)} tane {bolen} çarpanı vardır.')
