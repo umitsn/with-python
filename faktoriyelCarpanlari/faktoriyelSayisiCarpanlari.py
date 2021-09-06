17b12f4aeca416190c4792aac8ce8368e303742f
umitsn <49841290+umitsn@users.noreply.github.com>
2021-09-06 22:03:20 +0300

    Update faktoriyelSayisiCarpanlari.py

--- faktoriyelCarpanlari/faktoriyelSayisiCarpanlari.py
+++ faktoriyelCarpanlari/faktoriyelSayisiCarpanlari.py
@@ -1,10 +1,6 @@
 """
 ümit şen 09.2021 bircalismaningunlugu@gmail.com
-n! sayısının sonunda kaç 0 vardır ya da 
-n! içinde kaç tane x tamsayı çarpanı vardır
-sorusuna çözüm içindir.
-Bolen sayı 5 seçilirse n! sayısının sonunda kaç sıfır olduğu 
-bulunmuş olur.
+n! sayısının herhangi bir tamsayı böleninin sayısını hesaplar. Bolen sayı 5 seçilirse n! sayısının sonunda kaç sıfır bulunur.
 """
 sayi = 51
 bolen = 3
